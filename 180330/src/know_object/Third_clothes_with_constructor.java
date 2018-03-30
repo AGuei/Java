9e262744a2aa6c80770ad2a4edee9270f4fa0839
AGuei <k90418235@gcloud.csu.edu.tw>
2018-03-30 20:30:46 +0800

    新增第三件衣服與價格屬性

--- 180330/src/know_object/Third_clothes_with_constructor.java
+++ 180330/src/know_object/Third_clothes_with_constructor.java
@@ -3,18 +3,22 @@ package know_object;
 class Clothes2 {
     String color;
     char size;
-    Clothes2(String p1, char p2) {
+    int price;
+    Clothes2(String p1, char p2, int p3) {
         color = p1;
         size = p2;
+        price = p3;
     }
 }
 
 public class Third_clothes_with_constructor {
     public static void main(String[] args) {
-        Clothes2 sun = new Clothes2("red", 'S');
-        Clothes2 spring = new Clothes2("green", 'M');
+        Clothes2 sun = new Clothes2("red", 'S', 300);
+        Clothes2 spring = new Clothes2("green", 'M', 250);
+        Clothes2 winter = new Clothes2("black", 'L', 350);
         
-        System.out.printf("sun (%s, %c)%n", sun.color, sun.size);
-        System.out.printf("spring (%s, %c)%n", spring.color, spring.size);
+        System.out.printf("sun (%s, %c, %d¤¸)%n", sun.color, sun.size, sun.price);
+        System.out.printf("spring (%s, %c, %d¤¸)%n", spring.color, spring.size, spring.price);
+        System.out.printf("spring (%s, %c, %d¤¸)%n", winter.color, winter.size, winter.price);
     }
 }
