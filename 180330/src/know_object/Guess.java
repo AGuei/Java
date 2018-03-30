523a3213eab594809c70eb253cddb2a00f43d8b1
AGuei <k90418235@gcloud.csu.edu.tw>
2018-03-30 20:50:15 +0800

    新增提示使用者輸入數字是否過大或過小的功能以及統計猜測數字

--- 180330/src/know_object/Guess.java
+++ 180330/src/know_object/Guess.java
@@ -6,13 +6,21 @@ public class Guess {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int number = (int) (Math.random() * 10);
-        int guess;
+        int guess,count = 0;
         
         do {
             System.out.print("猜數字（0 ~ 9）:");
             guess = console.nextInt();
+            if(guess > number){
+            	System.out.println("再猜小一點");
+            }
+            else if(guess < number){
+            	System.out.println("再猜大一點");
+            }
+            count++;
         } while(guess != number);
         
         System.out.println("猜中了...XD");
+        System.out.printf("共猜了 %d 次",count);
     }
 }
