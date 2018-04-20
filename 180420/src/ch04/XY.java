4a4e1bb1bfd7241d21f106ee92b666991ce2b097
AGuei <k90418235@gcloud.csu.edu.tw>
2018-04-20 20:15:29 +0800

    改寫xy.java，求出C =  A + B

--- 180420/src/ch04/XY.java
+++ 180420/src/ch04/XY.java
@@ -2,15 +2,22 @@ package ch04;
 
 public class XY {
     public static void main(String[] args) {
-        int[][] cords = {
-            {1, 2, 3},
-            {4, 5, 6}
+        int[][] A = {
+            {3, 4, 2},
+            {1, 5, 7}
+        }, B = {
+            {-1, 3, 2},
+            {4, 2, -3}
+        },C = {
+        	{0, 0, 0}, 
+        	{0, 0, 0}
         };
-        for(int x = 0; x < cords.length; x++) {
-            for(int y = 0; y < cords[x].length; y++) {
-                System.out.printf("%2d", cords[x][y]);
+        for(int x = 0; x < A.length; x++) {
+            for(int y = 0; y < A[x].length; y++) {
+            	C[x][y] = A[x][y] + B[x][y];
+                System.out.printf("%2d", C[x][y]);
             }
-            System.out.println();
+            System.out.printf("\n");
         }
     }
 } 
