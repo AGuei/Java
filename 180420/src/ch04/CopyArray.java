1dfdc7b9f14a620f5d51b0898c31f9f489fc495e
AGuei <k90418235@gcloud.csu.edu.tw>
2018-04-20 21:00:49 +0800

    double space

--- 180420/src/ch04/CopyArray.java
+++ 180420/src/ch04/CopyArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CopyArray {
     public static void main(String[] args) {
         int[] scores1 = {88, 81, 74, 68, 78, 76, 77, 85, 95, 93};
-        int[] scores2 = Arrays.copyOf(scores1, scores1.length);
+        int[] scores2 = Arrays.copyOf(scores1, 2*scores1.length);
         for(int score : scores2) {
             System.out.printf("%3d", score);
         }
