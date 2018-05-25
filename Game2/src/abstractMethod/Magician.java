cb6a48d58ea2cd3e43637692afa09aa897733de3
AGuei <90418235@gcloud.csu.edu.tw>
2018-05-25 21:29:59 +0800

    父類別宣告抽象方法，強迫繼承的子類別要實作此抽象方法

---
+++ Game2/src/abstractMethod/Magician.java
@@ -0,0 +1,12 @@
+package abstractMethod;
+
+public class Magician extends Role {
+    @Override
+    public void fight() {
+        System.out.println("魔法攻擊");
+    }
+    
+    public void cure() {
+        System.out.println("魔法治療");
+    }
+}
