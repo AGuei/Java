4781383d91010d73f1ff7cecd59e7abfc2051847
AGuei <90418235@gcloud.csu.edu.tw>
2018-05-25 20:15:39 +0800

    在 RPG 顯示「劍士 (id) 揮劍攻擊，魔法師 (id) 魔法治療」

--- Game1/src/RPG.java
+++ Game1/src/RPG.java
@@ -9,8 +9,8 @@ public class RPG {
         swordsMan.setName("Justin");
         swordsMan.setLevel(1);
         swordsMan.setBlood(200);
-        System.out.printf("劍士：(%s, %d, %d)%n", swordsMan.getName(), 
-                swordsMan.getLevel(), swordsMan.getBlood());
+        System.out.printf("劍士 %s ", swordsMan.getName());
+        swordsMan.fight();
     }
 
     static void demoMagician() {
@@ -18,7 +18,7 @@ public class RPG {
         magician.setName("Monica");
         magician.setLevel(1);
         magician.setBlood(100);
-        System.out.printf("魔法師：(%s, %d, %d)%n", magician.getName(), 
-                magician.getLevel(), magician.getBlood());
+        System.out.printf("魔法師 %s ", magician.getName());
+        magician.cure();
     }
 }
