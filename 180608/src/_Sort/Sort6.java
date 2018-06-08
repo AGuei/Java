105cdf06c57410e115cee763e9abf86f60d444ba
AGuei <90418235@gcloud.csu.edu.tw>
2018-06-08 21:11:34 +0800

    sort initial

---
+++ 180608/src/_Sort/Sort6.java
@@ -0,0 +1,13 @@
+package _Sort;
+
+import java.util.*;
+import static java.util.Comparator.*;
+
+public class Sort6 {
+    public static void main(String[] args) {
+        List words = Arrays.asList(
+                "B", "X", "A", "M", null ,"F", "W", "O", null);
+        words.sort(nullsFirst(reverseOrder()));
+        System.out.println(words);
+    }
+}
