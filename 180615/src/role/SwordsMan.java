c53e1366cbad92e1052f12c6337a5fb1a64170d7
AGuei <90418235@gcloud.csu.edu.tw>
2018-06-15 20:27:14 +0800

    initial

---
+++ 180615/src/role/SwordsMan.java
@@ -0,0 +1,15 @@
+package role;
+
+public class SwordsMan extends Role {
+    @Override
+    public void fight() {
+        System.out.println("揮劍攻擊");
+    }
+    
+    @Override
+    public String toString() {
+        return "劍士 " + super.toString();
+    }
+
+
+}
